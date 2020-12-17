f1b06b0dddf7361de69764ab142dc6c05ae7a22a
nemo782 <76140621+nemo782@users.noreply.github.com>
2020-12-17 12:53:18 +0900

    practice

--- discordbot.py
+++ discordbot.py
@@ -21,4 +21,19 @@ async def ping(ctx):
 async def neko(ctx):
     await ctx.send('にゃーん')
     
+    if message.author.bot:
+        return
+    # 「/もりちゃん」と発言したら「ﾃﾞｭﾌﾌ」が返る処理
+    if message.content == '/もりちゃん':
+        await message.channel.send('???(ﾃﾞｭﾌﾌ)')
+
+    # 「/やおさん」と発言したら「以下の文章」が返る処理
+    if message.content == '/やおさん':
+        await message.channel.send('ﾝﾝ紫アーマーで殴ってくんのは違うじゃん！！')
+
+    # 「/apex character」と発言したら「キャラガチャ」が始まる処理
+    if message.content == '/apex character':
+        character = ["ブラハ","ジブ","ライフラ","パスファ","レイス","バンガ","ミラージュ","コースティック","オクタン","ワットソン","クリプト","レヴナント","ローバー","ランパート","ホライゾン" ]
+        character_number = random.randint(0,14)
+        await message.channel.send("今回は" + character[character_number] + "に決定！")
 bot.run(token)
